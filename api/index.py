083fe8f8a1e9f3c2fc12caf59f072f0d5a9ac4d1
Jason Fotta <cyaaroughies@gmail.com>
2026-02-01 20:18:17 +0000

    Fix indentation error in CORS config

--- api/index.py
+++ api/index.py
@@ -24,10 +24,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 # ---------- Paths ----------
